e184b9aff05f9d197654a4c0e8ba9608ec51f55b
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 18:52:29 -0500

    Changing the h1 to page title

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,12 @@ from .models import Link
 
 @app.route('/')
 def index():
-    application_name = {'name': 'My Links'}
+    # application_name = {'name': 'My Links'}
+    page_name = 'Link list'
     links = Link.query.all()
-    return render_template ('index.html', links=links, application_name=application_name)
+    return render_template ('index.html', links=links, application_name=page_name)
 
 @app.route('/add')
 def add():
-    application_name = {'name': 'My Links'}
-    return render_template ('add.html', application_name=application_name)
+    page_name = 'Add a link'
+    return render_template ('add.html', application_name=page_name)
