4da6ef367ff9a12a496ccafcb7cddf296cc4f244
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 18:42:00 -0500

    Add route and template added

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,8 @@ def index():
     application_name = {'name': 'My Links'}
     links = Link.query.all()
     return render_template ('index.html', links=links, application_name=application_name)
+
+@app.route('/add')
+def add():
+    application_name = {'name': 'My Links'}
+    return render_template ('add.html', application_name=application_name)
