788d0172d9f7f7eaf01b22b3cc62f5ee12bfe1d4
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 13:48:04 -0500

    New database fields

--- app/models.py
+++ app/models.py
@@ -6,12 +6,16 @@ class Link(db.Model):
     starred = db.Column(db.Boolean)
     read = db.Column(db.Boolean)
     dead_link = db.Column(db.Boolean)
+    header = db.Column(db.Boolean)
+    space = db.Column(db.Boolean)
 
-    def __init__(self, url, starred=False, read=False, dead_link=False):
+    def __init__(self, url="", starred=False, read=False, dead_link=False, header=False, space=False):
         self.url=url
         self.starred = starred
         self.read = read
         self.dead_link = dead_link
+        self.header = header
+        self.space = space
         
     def __repr__(self):
         return '<URL {}>'.format(self.url)
