73d4a9ba0993a25f3b0b5583e180fe99233fa6cb
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 10:24:03 -0500

    Correctly pulls from text and displays in template

--- app/Controllers/stuff.py
+++ app/Controllers/stuff.py
@@ -3,30 +3,29 @@ class Entry:
         self.url = url
         self.starred = starred
         self.read = read
-    
 
-inputFile = open("sample_txt_file.txt", "r")
+# try:
+#     outputFile = open("sample_txt_file2.txt", "x")
+# except:
+#     outputFile = open("sample_txt_file2.txt", "w")
 
-try:
-    outputFile = open("sample_txt_file2.txt", "x")
-except:
-    outputFile = open("sample_txt_file2.txt", "w")
+def getLinkList():
+    inputFile = open("sample_txt_file.txt", "r")
+    linkList = []
+    inputFile.seek(0)
+    for line in inputFile:
+        if line != "\n":
+            # outputFile.write(line)
+            real_line = ""
+            for x in line:
+                if x != "\n":
+                    real_line += x
+            if "google" in real_line:
+                entry = Entry(real_line, True)
+            else:
+                entry = Entry(real_line)
+            linkList.append(entry)
+    inputFile.close
+    return linkList
 
-inputFile.seek(0)
-for line in inputFile:
-    if line != "\n":
-        outputFile.write(line)
-        real_line = ""
-        for x in line:
-            if x != "\n":
-                real_line += x
-        if "google" in real_line:
-            entry = Entry(real_line, True)
-        else:
-            entry = Entry(real_line)
-        print(entry.url)
-        print(entry.starred)
-        print(entry.read)
-
-inputFile.close
-outputFile.close
+# outputFile.close
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
 from flask import render_template
 
+from app.Controllers.stuff import getLinkList
+
 @app.route('/')
 def index():
-    # return "Hello, world!"
-    application_name = {'name': 'Links'}
-    return render_template ('index.html', application_name=application_name)
+    application_name = {'name': 'My Links'}
+    links=getLinkList()
+    return render_template ('index.html', application_name=application_name, links=links)
