a4dc4b73d39a6d4a9b7fcdf690b14dffb9acfcce
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 12:47:17 -0500

    routes cleanup

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,6 @@ from flask import render_template
 
 from .models import Link
 
-from app.Controllers.stuff import populateDatabase
-
 @app.route('/')
 def index():
     application_name = {'name': 'My Links'}
