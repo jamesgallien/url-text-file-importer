e338e1cfb8bac7db5c9d0e401a18c2354aa4b0fc
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 12:44:27 -0500

    Removing intermediate linkList functionality

--- app/Controllers/stuff.py
+++ app/Controllers/stuff.py
@@ -5,7 +5,6 @@ from ..models import Link
 
 def populateDatabase():
     inputFile = open("sample_txt_file.txt", "r")
-    linkList = []
     inputFile.seek(0)
     for line in inputFile:
         if line != "\n":
@@ -15,23 +14,13 @@ def populateDatabase():
                     real_line += x
             if real_line[0] == "*":
                 real_line = real_line[1:]
-                entry = Entry(real_line, True)
                 link = Link(url=real_line, starred=True)
                 db.session.add(link)
                 db.session.commit()
             else:
-                entry = Entry(real_line)
                 link = Link(url=real_line)
                 db.session.add(link)
                 db.session.commit()
-            print(link.id)
-            print(link.url)
-            print(link.starred)
-            print(link.read)
-            print(link.dead_link)
-            linkList.append(entry)
-    for link in Link.query.all():
-        print(link)
     inputFile.close
 
 def clearDatabase():
@@ -42,5 +31,3 @@ def clearDatabase():
 def printDatabase():
     for link in Link.query.all():
         print(link)
-
-# outputFile.close
