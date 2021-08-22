a4b1cee089c8acc206101f1c28413ecac8ddd725
James Gallien <James.E.Gallien@nga.mil>
2021-08-21 20:17:58 -0500

    Minor cleanup

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ from .forms import AddForm
 
 @app.route('/')
 def index():
-    # application_name = {'name': 'My Links'}
     page_name = 'Link list'
     links = Link.query.all()
     return render_template ('index.html', links=links, application_name=page_name)
@@ -16,7 +15,6 @@ def add():
     page_name = 'Add a link'
     form = AddForm()
     if form.validate_on_submit():
-        # flash('Add requested for link: {}'.format(form.link.data))
         link=Link(url=form.link.data, starred=form.starred.data, read=form.read.data,
             dead_link=form.dead_link.data, header=form.header.data)
         db.session.add(link)
